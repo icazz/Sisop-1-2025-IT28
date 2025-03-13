81bf93be156221d1fb347ebb58b497c6bae5c848
ZIKA <icha.zika.1@gmail.com>
2025-03-13 22:05:06 +0700

    revisi dsotm.sh

--- soal_3/dsotm.sh
+++ soal_3/dsotm.sh
@@ -3,15 +3,28 @@
 clear
 
 speak_to_me(){
+    tput civis
     while true; do
-        affirmation=$(curl -s https://www.affirmations.dev | sed -n 's/.*"affirmation":"\(.*\)".*/\1/p')
+        affirmation=$(curl -s https://www.affirmations.dev | sed -E 's/\{"affirmation":"//; s/"\}//')
         echo -e "$affirmation"
         sleep 1
     done
 }
 
 on_the_run(){
-    ./../progress_bar.sh
+    echo "Ready, set, go!"
+    length=$(($(tput cols) - 7))
+    [ $length -lt 10 ] && length=10
+
+    for i in $(seq 1 100); do
+        sleep 0.1
+
+        progress=$((i * length / 100))
+        progress_bar=$(printf "%0.s#" $(seq 1 $progress))
+
+        printf "\r[%-${length}s] %3d%%" "$progress_bar" "$i"
+    done
+    echo "" 
 }
 
 time_function(){
@@ -24,7 +37,44 @@ time_function(){
 }
 
 money(){
-    ./../hujan.sh
+    symbols=('€' '£' '¥' '¢' '₹')
+    colors=(5 6 7)
+    clear
+
+    cols=$(tput cols)
+    rows=$(tput lines)
+    declare -A positions
+
+    for ((i = 0; i < cols; i++)); do
+        positions[$i]=$((RANDOM % rows))
+    done
+
+    # Loop animasi
+    while true; do
+        for ((i = 0; i < cols / 2; i++)); do
+            col=$((RANDOM % cols))
+
+            symbol="${symbols[RANDOM % ${#symbols[@]}]}"
+            color="${colors[RANDOM % ${#colors[@]}]}"
+
+            tput cup "${positions[$col]}" "$col"
+            echo " "
+
+            new_pos=$((positions[$col] - 2))
+            if (( new_pos < 0 )); then new_pos=$rows; fi
+            positions[$col]=$new_pos
+
+            # Set warna
+            tput setaf $color
+
+            tput cup "$new_pos" "$col"
+            echo -n "$symbol"
+
+            # Reset warna
+            tput sgr0
+        done
+        sleep 0.00000001
+done
 }
 
 brain_damage(){
